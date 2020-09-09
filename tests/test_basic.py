1e57c01e5aa2d17ae8d7795a353edb21ff3ebd54
Silvio Tomatis <silviot@gmail.com>
2020-09-09 16:57:40 +0200

    Refactoring

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,11 +5,18 @@ import os
 import tempfile
 
 
+CREATE_VIDEO = False
+
+
 def test_first():
     specs = Specs(Path(__file__).parent / "test-files" / "example.json")
     fh, tmp_path = tempfile.mkstemp(".mp4")
     os.fdopen(fh).close()
     os.unlink(tmp_path)
     final = specs.get_final(tmp_path)
-    final.run()
-    print(f"Written file {tmp_path}")
+    if CREATE_VIDEO:
+        final.run()
+        print(f"Written file {tmp_path}")
+    final.compile()  # Smoke test
+    # Uncomment the next line to see the rendering plan graph
+    # final.view()
--- videostitcher/specs.py
+++ videostitcher/specs.py
@@ -1,8 +1,10 @@
+from ffmpeg.nodes import FilterableStream
 from ffmpeg.nodes import OutputStream
 from pathlib import Path
 from pendulum import DateTime
 from pendulum import from_timestamp
 from pendulum import Period
+from typing import Dict
 
 import ffmpeg
 import json
@@ -14,15 +16,53 @@ class Specs:
     Instantiate passing the JSON file path as the only argument to `__init__`.
     """
 
+    #: A dictionary read (and slightly augmented/changed) from the config file
+    config: Dict
+    #: Pendulum Period indicating the time span the output should show
+    output_period: Period
+    #: Tracks needed to assemble the output
+    tracks: Dict[str, FilterableStream]
+    #: The audio track of the output
+    audio_track: FilterableStream
+
     def __init__(self, filepath: str):
         self.filepath = Path(filepath)
         with open(filepath) as fh:
             self.config = json.load(fh)
+        output_start = parse_ts(int(self.config["output_start"]))
+        output_end = output_start.add(seconds=self.config["output_duration"])
+        self.output_period = output_end - output_start
+        self.prepare_inputs()
+
+    def prepare_inputs(self):
+        """Prepare the input tracks needed to assemble the output.
+        Store the result in self.tracks
+        """
+        self.tracks = {}
         self.inputs = {}
-        for fileinfo in self.config["inputs"]:
-            fileinfo["filename"] = self.absolute_path(fileinfo["filename"])
-            print(f"Analyzing {fileinfo['filename']}", file=sys.stderr)
-            self.inputs[fileinfo["streamname"]] = ffmpeg.probe(fileinfo["filename"])
+        for input_info in self.config["inputs"]:
+            # Convert file paths to absolute in case they're relative
+            # TODO we can support HTTP URLs by prepending async:cache
+            # see https://ffmpeg.org/ffmpeg-protocols.html#async
+            input_info["filename"] = self.absolute_path(input_info["filename"])
+            print(f"Analyzing {input_info['filename']}", file=sys.stderr)
+            # Use ffprobe to get more info about streams
+            input_file_info = ffmpeg.probe(input_info["filename"])
+
+            input_start = parse_ts(int(input_info["start"]))
+            input_duration = float(input_file_info["format"]["duration"])
+            input_end = input_start.add(seconds=input_duration)
+            input_period = input_end - input_start
+            audio_streams = []
+            if overlaps(self.output_period, input_period):
+                self.tracks[input_info["streamname"]] = adjust_track(
+                    input_info, self.output_period
+                )
+            if has_audio(input_file_info["streams"]):
+                audio_streams.append(adjust_audio_track(input_info, self.output_period))
+        self.audio_track = ffmpeg.filter(
+            audio_streams, "amix", inputs=len(audio_streams)
+        )
 
     def absolute_path(self, filename: str) -> str:
         """If the passed in file path is not absolute, convert it to absolute,
@@ -38,64 +78,47 @@ class Specs:
         """Returns an OutputStream object representing the work needed to produce the
         final output. Useful methods on that object are `view()` (shows a graphical
         representation of the video processing graph) and `run()` (actually produces the file)"""
-        output_start = parse_ts(int(self.config["output_start"]))
-        output_duration = self.config["output_duration"]
-        output_end = output_start.add(seconds=output_duration)
-        output_period = output_end - output_start
-        inputs = {}
-        audio_streams = []
-        for input_info in self.config["inputs"]:
-            input_start = parse_ts(int(input_info["start"]))
-            input_file_info = self.inputs[input_info["streamname"]]
-            input_duration = float(input_file_info["format"]["duration"])
-            input_end = input_start.add(seconds=input_duration)
-            input_period = input_end - input_start
-
-            # Only append this to the inputs if it contributes to the final video.
-            if overlaps(output_period, input_period):
-                # stream_delay is the length of time between the start of this stream and te start of the output
-                # A positive value means this input should be trimmed, and starts as the output starts.
-                # A negative value means this input should be delayed, and starts at a later point than the main output.
-                stream_delay = (
-                    output_start - parse_ts(input_info["start"])
-                ).in_seconds()
-                input = ffmpeg.input(input_info["filename"])
-                if stream_delay > 0:
-                    input = input.filter("trim", start=stream_delay).filter(
-                        "setpts", "PTS-STARTPTS"
-                    )
-                    print(
-                        f"Stream {input_info['streamname']} starts before main start: trimming",
-                        file=sys.stderr,
-                    )
-                elif stream_delay < 0:
-                    input = input.filter("setpts", "PTS-STARTPTS+{-stream_delay}")
-                    print(
-                        f"Stream {input_info['streamname']} starts after main start: delaying",
-                        file=sys.stderr,
-                    )
-                inputs[input_info["streamname"]] = input
-                if has_audio(input_file_info["streams"]):
-                    audio = ffmpeg.input(input_info["filename"]).audio
-                    if stream_delay > 0:
-                        audio_streams.append(
-                            audio.filter("atrim", start=stream_delay).filter(
-                                "asetpts", "PTS-STARTPTS"
-                            )
-                        )
-                    else:
-                        audio_streams.append(
-                            audio.filter("setpts", "PTS-STARTPTS+{-stream_delay}")
-                        )
-            else:
-                print(f"Discarding input {input_info['streamname']}")
-
         size = "{width}:{height}".format(**self.config["output_size"])
-        mixed_audio = ffmpeg.filter(audio_streams, "amix", inputs=len(audio_streams))
-        in_video = inputs["video-vertical-phone"].filter(
+        in_video = self.tracks["video-vertical-phone"].filter(
             "scale", size=size, force_original_aspect_ratio="increase"
         )
-        return mixed_audio.output(in_video, destination, t=output_duration)
+        return self.audio_track.output(
+            in_video, destination, t=self.output_period.in_seconds()
+        )
+
+
+def adjust_track(input_info: Dict[str, str], output_period: Period) -> FilterableStream:
+    """Adjust a track so that it has the same duration as the output period"""
+    # stream_delay is the length of time between the start of this stream and te start of the output
+    # A positive value means this input should be trimmed, and starts as the output starts.
+    # A negative value means this input should be delayed, and starts at a later point than the main output.
+    stream_delay = (
+        output_period.start - parse_ts(int(input_info["start"]))
+    ).in_seconds()
+    input = ffmpeg.input(input_info["filename"])
+    if stream_delay > 0:
+        input = input.filter("trim", start=stream_delay).filter(
+            "setpts", "PTS-STARTPTS"
+        )
+    elif stream_delay < 0:
+        input = input.filter("setpts", "PTS-STARTPTS+{-stream_delay}")
+    return input
+
+
+def adjust_audio_track(
+    input_info: Dict[str, str], output_period: Period
+) -> FilterableStream:
+    """Adjust an audio track to match desired output times"""
+    stream_delay = (
+        output_period.start - parse_ts(int(input_info["start"]))
+    ).in_seconds()
+    audio = ffmpeg.input(input_info["filename"]).audio
+    if stream_delay > 0:
+        return audio.filter("atrim", start=stream_delay).filter(
+            "asetpts", "PTS-STARTPTS"
+        )
+    else:
+        return audio.filter("setpts", "PTS-STARTPTS+{-stream_delay}")
 
 
 def parse_ts(ts: int) -> DateTime:
